3fe689a55e01900ef1ec15dbcd464c771646a920
Rémy M <niverton@live.fr>
2017-03-27 17:05:05 +0200

    Nouveaux paramètres de carte de disparité (ne marche toujours pas sur les photos du robot

--- opencv-plugin/auto/src/disparityprocessor.cpp
+++ opencv-plugin/auto/src/disparityprocessor.cpp
@@ -5,6 +5,32 @@
 using namespace cv;
 
 DisparityProcessor::DisparityProcessor() {
+    /* Paramètres testés en simulation et jugés bon:
+		Pre filter clip: 0
+		Sad window size: 21
+		Minimum disparity: -3
+		Number of disparity: 64
+		Texture threshomd: 0
+		Uniqueness ratio: 0
+		Speckle size: 79
+		Speckle range: 2
+		Disparity Max difference: -1
+	*/
+
+    //* Defaut Jeremi
+    prop.minDisparity = 0;
+    prop.numDisparity = 64;
+    prop.SadWindowSize = 21;
+    prop.P1 = 8 * prop.SadWindowSize * prop.SadWindowSize;
+    prop.P2 = 32 * prop.SadWindowSize * prop.SadWindowSize;
+    prop.disp12MaxDiff = -1;
+    prop.preFilterCap = 31;
+    prop.uniquenessRatio = 0;
+    prop.speckleWindowSize = 0;
+    prop.fullDP = false;
+    //*/
+
+    /* Defaut Hadrien
     prop.minDisparity = 0;
     prop.numDisparity = 64;
     prop.SadWindowSize = 21;
@@ -15,6 +41,7 @@ DisparityProcessor::DisparityProcessor() {
     prop.uniquenessRatio = 15;
     prop.speckleWindowSize = 0;
     prop.fullDP = false;
+    //*/
 }
 
 void DisparityProcessor::process(Mat& img, const std::string& fileName) {
